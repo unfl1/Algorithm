a8d83efd49574d1295079d38077367e7e5179d1b
unfl1 <flxmaut10@gmail.com>
2026-09-10 14:20:51 +0900

    가장 큰 수

--- programmers/C++/Lv2/가장 큰 수/main.cpp
+++ programmers/C++/Lv2/가장 큰 수/main.cpp
@@ -12,8 +12,9 @@ string solution(vector<int> numbers) {
       numb.push_back(to_string(num));
     }
 
+    // 둘을 앞뒤로 붙여보고, 더 큰 쪽이 앞에 오게 정렬한다
     sort(numb.begin(), numb.end(), [] (string a, string b){
-      return a+a+a+a > b+b+b+b;
+      return a+b > b+a;
     });
 
     for (string s : numb){
