d31e82497600e567c400ace23edf1af4bc27e760
unfl1 <flxmaut10@gmail.com>
2026-09-21 23:29:49 +0900

    합승 택시 요금

--- programmers/Python/Lv3/합승 택시 요금.py
+++ programmers/Python/Lv3/합승 택시 요금.py
@@ -9,6 +9,8 @@ def solution(n, s, a, b, fares):
         graph[start].append((end, cost))
         graph[end].append((start, cost))
 
+    # 둘이 s에서 출발해서 어떤 지점 k까지 같이 탔다가
+    # 거기서 갈라진다고 하면 총 비용은 dist(s, k) + dist(k, a) + dist(k, b)
     dist_s = dijkstra(s, n, graph)
     dist_a = dijkstra(a, n, graph)
     dist_b = dijkstra(b, n, graph)
@@ -16,10 +18,7 @@ def solution(n, s, a, b, fares):
     answer = INF
 
     for k in range(1, n + 1):
-        answer = min(
-            answer,
-            dist_s[k] + dist_a[k] + dist_b[k]
-        )
+        answer = min(answer, dist_s[k] + dist_a[k] + dist_b[k])
 
     return answer
 
