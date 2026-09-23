f7cdd9c364d6ac4ffe61b49ad11e1e5e8bd5420f
unfl1 <flxmaut10@gmail.com>
2026-09-23 15:06:50 +0900

    이중순위우선큐

---
+++ programmers/Python/Lv3/이중순위우선큐.py
@@ -0,0 +1,27 @@
+import heapq
+
+def solution(operations):
+    max_pq = []
+    min_pq = []
+    
+    for op in operations:
+        a, b = op.split()
+        
+        if a=="I":
+            heapq.heappush(max_pq, -int(b))
+            heapq.heappush(min_pq, int(b))
+            
+        elif a=="D":
+            if b=="1":
+                if max_pq:
+                    val = -heapq.heappop(max_pq)
+                    min_pq.remove(val)
+            else:
+                if min_pq:
+                    val = -heapq.heappop(min_pq)
+                    max_pq.remove(val)
+                    
+    if min_pq:
+        return [max(min_pq), min(min_pq)]
+    else:
+        return [0,0]
