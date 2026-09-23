01ad90360b45fafd2641c87dcdb04e368ecaf4e1
unfl1 <flxmaut10@gmail.com>
2026-09-23 15:52:58 +0900

    이중순위우선큐

--- programmers/Python/Lv3/이중순위우선큐.py
+++ programmers/Python/Lv3/이중순위우선큐.py
@@ -16,10 +16,12 @@ def solution(operations):
                 if max_pq:
                     val = -heapq.heappop(max_pq)
                     min_pq.remove(val)
+                    heapq.heapify(min_pq)
             else:
                 if min_pq:
                     val = -heapq.heappop(min_pq)
                     max_pq.remove(val)
+                    heapq.heapify(max_pq)
                     
     if min_pq:
         return [max(min_pq), min(min_pq)]
