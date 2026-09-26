6e036b656f723b25201d1290909b09f7d5a5730f
unfl1 <flxmaut10@gmail.com>
2026-09-26 15:07:25 +0900

    H-Index

---
+++ programmers/Python/Lv2/H-Index.py
@@ -0,0 +1,9 @@
+def solution(citations):
+    citations.sort(reverse=True)
+    h_index = 0
+    
+    for c in citations:
+        if c > h_index:
+            h_index += 1
+            
+    return h_index
