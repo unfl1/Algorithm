b77c1f02a17b32a50a61bcccf500160c8fce074f
unfl1 <flxmaut10@gmail.com>
2026-09-27 01:54:06 +0900

    기능개발

--- programmers/Python/Lv2/기능개발.py
+++ programmers/Python/Lv2/기능개발.py
@@ -1,18 +1,18 @@
 def solution(progresses, speeds):
-    answer=[]
+    answer = []
     cur=0
 
-    while (cur!=len(speeds)):
-        for i in range(cur, len(speeds)):
+    while cur<len(progresses):
+        for i in range(len(progresses)):
             progresses[i]+=speeds[i]
+        
+        cnt = 0
 
-        cnt=0
-
-        while (cur<len(progresses) and progresses[cur]>=100):
-            cnt+=1
+        while cur<len(progresses) and progresses[cur]>=100:
             cur+=1
-
-        if cnt > 0:
+            cnt+=1
+            
+        if cnt>0:
             answer.append(cnt)
-
+        
     return answer
